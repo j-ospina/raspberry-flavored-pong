c0e825609356044b17661ffe81fc4bfa5a61ad0d
AustinHarrison1992 <104733222+SV-Engineer@users.noreply.github.com>
2022-05-12 15:53:06 -0500

    Updated lesson 0 to be easier.

---
+++ PythonLessons/Lesson0/main.py
@@ -0,0 +1,14 @@
+# Assignment:
+petName = ""
+print("My pet's name is " + petName)
+petName = ""
+print("My pet's name is " + petName)
+
+# Bonus points:
+# In the following code, myName() is a _______?
+# In the following code, on line 13, the left side is a _______?
+# In the following code, on line 13, the right side is a _______?
+
+def myName():
+    a = "Burrell"
+    print(a)
